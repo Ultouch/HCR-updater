b19089740563f0a5d5309584ded10270688ae970
huihuicloud <chenrichardit@gmail.com>
2023-04-15 13:04:50 +0200

    maj

---
+++ main.js
@@ -0,0 +1,62 @@
+const {app, BrowserWindow, Menu, ipcMain} = require('electron')
+const url = require('url')
+const path = require('path');
+
+const options = true;
+
+Menu.setApplicationMenu(false)
+
+function createWindow () { 
+    if(options == true){
+        const mainWindow = new BrowserWindow({
+            show: false,
+            height: 440,
+            width: 753,
+            resizable: false,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true
+            },
+            frame: false,
+            maximizable: false,
+            minimizable: false,
+            title: 'HCR',
+        });
+        mainWindow.loadURL('http://localhost:4200')
+        mainWindow.once('ready-to-show', () => {
+            mainWindow.show();
+        });
+    }else{
+        const mainWindow = new BrowserWindow({
+            show: false,
+            height: 440,
+            width: 753,
+            resizable: false,
+            webPreferences: {
+                nodeIntegration: true,
+                enableRemoteModule: true
+            },
+            frame: false,
+            maximizable: false,
+            minimizable: false,
+            title: 'HCR',
+        });
+        mainWindow.loadURL(url.format({pathname: path.join(__dirname, `/dist/hcr-updater/index.html`),protocol: "file:",slashes: true}));
+        mainWindow.once('ready-to-show', () => {
+            mainWindow.show();
+        });
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.allowRendererProcessReuse = false
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hcr-updater';
+
+  connected:number = 0;
+
+  ngAfterViewInit(){
+    
+  }
 }
